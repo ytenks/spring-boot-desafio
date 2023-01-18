b75a61cc4079d9684a9f6afe85fa2b4b2c97329f
tenks <arturedmundo@Hotmail.com>
2023-01-17 21:52:12 -0300

    Projeto criado

---
+++ src/main/java/com/meta3/desafiometa3/Desafiometa3Application.java
@@ -0,0 +1,13 @@
+package com.meta3.desafiometa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiometa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafiometa3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/meta3/desafiometa3/Desafiometa3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta3.desafiometa3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafiometa3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
