c5a69c9a4b67406b730831ae9dfa817501b52c6e
tenks <arturedmundo@Hotmail.com>
2023-01-20 00:34:18 -0300

    adicionando a tabela tipo post

--- src/main/java/com/meta3/desafiometa3/classes/Category.java
+++ src/main/java/com/meta3/desafiometa3/classes/Category.java
@@ -1,13 +1,16 @@
 package com.meta3.desafiometa3.classes;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +22,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Post> posts = new HashSet<>();
+	
 	public Category() {
 	}
 	
@@ -41,6 +48,10 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 	
+	public Set<Post> getPosts() {
+		return posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -56,6 +67,8 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 	
 
 }
--- src/main/java/com/meta3/desafiometa3/classes/Post.java
+++ src/main/java/com/meta3/desafiometa3/classes/Post.java
@@ -1,13 +1,17 @@
 package com.meta3.desafiometa3.classes;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
 @Entity
 @Table(name="tb_post")
 public class Post implements Serializable {
@@ -18,30 +22,43 @@ public class Post implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private String description;
+	@Transient
+	private Set<Category> categories= new HashSet<>();
+	
 	public Post() {
-		super();
 	}
+
 	public Post(Long id, String description) {
 		super();
 		this.id = id;
 		this.description = description;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getDescription() {
-		return description;
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
